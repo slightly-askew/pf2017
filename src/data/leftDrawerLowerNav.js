7b12c82fd926ac902302a469acb2dc4cc601b478
Slightly <oaskew@gmail.com>
2017-04-29 12:11:17 +1000

    chore: comment vCard import to naively work with storybook

--- src/data/leftDrawerLowerNav.js
+++ src/data/leftDrawerLowerNav.js
@@ -1,8 +1,8 @@
 
-import vCard from '../assets/vCard.vcf'
+//import vCard from '../assets/vCard.vcf'
 
 export default [
-  {"VCard": ["Download a vCard", `data:text/vcard;${vCard}`]},
+  {"VCard": ["Download a vCard", `data:text/vcard;${/*vCard*/console.log('vCard needs to be imported, switched off to make storybook work')}`]},
   {"LinkedIn": ["My LinkedIn Profile","https://www.linkedin.com/in/oliver-askew-5791a333/"]},
   {"Sms": ["Send a text", "sms://+610403066036"]}
 ]
